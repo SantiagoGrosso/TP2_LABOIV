659724050ec0ca09bfde5f8c4765a201a79361c3
SantiagoGrosso <santigrosso107@gmail.com>
2024-06-13 10:58:09 -0300

    cambios

---
+++ TP2/src/enviroments/enviroments.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyDGif3NJXC7v7VKBGL1rhq_yiAQKG94WfU",
+        authDomain: "tp02-ef411.firebaseapp.com",
+        projectId: "tp02-ef411",
+        storageBucket: "tp02-ef411.appspot.com",
+        messagingSenderId: "1038947319102",
+        appId: "1:1038947319102:web:c95224758351e20770806f"
+    },
+    production: true,
+    firebaseConfig: 
+    {
+        apiKey: "AIzaSyDGif3NJXC7v7VKBGL1rhq_yiAQKG94WfU",
+        authDomain: "tp02-ef411.firebaseapp.com",
+        projectId: "tp02-ef411",
+        storageBucket: "tp02-ef411.appspot.com",
+        messagingSenderId: "1038947319102",
+        appId: "1:1038947319102:web:c95224758351e20770806f"
+    }
+  };
